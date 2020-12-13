3f838c8e543e30248a8d24327ee5028c6c7b5fd0
dori <dv49899@fer.hr>
2020-12-14 00:50:53 +0100

    beginning

--- out/app.js
+++ out/app.js
@@ -1,18 +1,58 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+#!/usr/bin/env node
+'use strict';
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = express_1.default();
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('The sedulous hyena ate the antelope!');
+//import { response } from "express";
+const coingecko = require('coingecko-api');
+const ciongeckoclient = new coingecko();
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
-app.listen(port, () => {
-    if (Error) {
-        return console.error(Error);
-    }
-    return console.log(`server is listening on ${port}`);
+// var func = async() => {
+//     let data = await ciongeckoclient.ping();
+//     console.log(data);
+// }
+// func();
+//token/coin/cryptocurency input
+var curency = "";
+const input = () => {
+    return new Promise((resolve, reject) => {
+        rl.question('Please enter the name of a token/coin/cryptocurency (ID or symbol)\n or \"quit\" to exit program.\n\n\n', (answer) => {
+            if (answer == 'quit')
+                process.exit(0);
+            curency = answer;
+            // console.log(answer);
+            // console.log(curency);
+            resolve();
+        });
+    });
+};
+//logic (sve stavit u petlju)
+var resultExchange = "";
+var resultPrice = "";
+curency = curency.toLowerCase();
+//output of results
+var output = () => {
+    console.log(resultExchange + ' ' + resultPrice + '\n');
+};
+//main function
+const main = () => __awaiter(void 0, void 0, void 0, function* () {
+    yield input();
+    rl.close();
+    output();
 });
+//execution
+var i;
+for (i = 0; i < 2; i++) {
+    main();
+}
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,59 @@
-import express from 'express';
+#!/usr/bin/env node
 
-const app = express();
+'use strict'
 
+//import { response } from "express";
+
+const coingecko = require('coingecko-api');
+const ciongeckoclient = new coingecko();
+
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+    // var func = async() => {
+    //     let data = await ciongeckoclient.ping();
+    //     console.log(data);
+    // }
+
+    // func();
+
+//token/coin/cryptocurrency input
+var curency:string = "";
+
+const input = () => {
+    return new Promise<void>((resolve, reject) => {
+      rl.question('Please enter the name of a token/coin/cryptocurrency (ID or symbol)\n or \"quit\" to exit program.\n\n\n', (answer:string) => {
+        if(answer == 'quit') process.exit(0);
+        curency = answer;
+        // console.log(answer);
+        // console.log(curency);
+        resolve();
+      })
+    })
+  }
+
+//logic (sve stavit u petlju)
+var resultExchange:string = "";
+var resultPrice:number = 0;
+
+curency = curency.toLowerCase();
+
+
+
+//output of results
+var output = () => {
+    console.log(resultExchange + ' ' + resultPrice + '$' + '\n');
+}
+
+//main function
+const main = async () => {
+await input();
+rl.close();
+output();
+}
+
+//execution
+main();
