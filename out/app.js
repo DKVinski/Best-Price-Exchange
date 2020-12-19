b67a9059acff5b26c257ba0bffeaeef1bd00065a
dori <dv49899@fer.hr>
2020-12-19 21:10:51 +0100

    to-do logic

--- out/app.js
+++ out/app.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-'use strict';
+"use strict";
+//token/coin/cryptocurrency input
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -9,119 +10,155 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const coingecko = require('coingecko-api');
-const ciongeckoclient = new coingecko();
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-//token/coin/cryptocurrency input
-var curency = "";
-var resultExchange = "";
-var resultPrice = 0;
-var exchangesNumber = 0;
-var coinsNumber = 0;
-var exchangesList = new Array();
-var coinsList = new Array();
-const input = () => {
-    return new Promise((resolve, reject) => {
-        rl.question('Please enter the name of a token/coin/cryptocurrency (ID or symbol)\n or \"quit\" to exit program.\n\n\n', (answer) => {
-            if (answer == 'quit')
-                process.exit(0);
-            curency = answer.toLowerCase();
-            resolve();
-        });
+const input = () => __awaiter(void 0, void 0, void 0, function* () {
+    'use strict';
+    var curency = "";
+    const readline = require('readline');
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
     });
-};
-const inputCheck = (input) => __awaiter(void 0, void 0, void 0, function* () {
-    var isValid = false;
-    var i;
-    for (i = 0; i < coinsNumber; i++) {
-        if (curency == coinsList[i].symbol.toLowerCase()) {
-            isValid = true;
-            curency = coinsList[i].id.toLowerCase();
-            break;
-        }
-        else if (curency == coinsList[i].id.toLowerCase()) {
-            isValid = true;
-            break;
-        }
-    }
-    if (isValid = false) {
-        console.log('Input is not valid cryptocurrency/token/coin.\n');
-    }
+    const questionn = () => {
+        return new Promise((resolve, reject) => {
+            rl.question('Please enter the name of a token/coin/cryptocurrency (ID or symbol)\n or \"quit\" to exit program.\n\n\n', (answer) => {
+                if (answer == 'quit') {
+                    process.exit(0);
+                }
+                ;
+                curency = answer.toLowerCase();
+                resolve();
+            });
+        });
+    };
+    console.log(yield questionn());
+    rl.close();
+    return curency;
 });
-var exchangeList = () => __awaiter(void 0, void 0, void 0, function* () {
+//checks if input is valid and returns coin Id
+const inputIdCheck = (input, coinsIdList, coinsSymbolList) => __awaiter(void 0, void 0, void 0, function* () {
+    return new Promise(() => {
+        var isValid = false;
+        let i;
+        for (i = 0; i < coinsIdList.length; i++) {
+            if (input == coinsSymbolList[i].toLowerCase()) {
+                isValid = true;
+                input = coinsIdList[i].toLowerCase();
+                return input;
+            }
+            else if (input == coinsIdList[i].toLowerCase()) {
+                isValid = true;
+                return input;
+            }
+        }
+        if (isValid == false) {
+            console.log('Input is not valid cryptocurrency/token/coin.\n');
+            return '';
+        }
+    });
+});
+//checks if input is correct and returns coin symbol
+const inputSymbolCheck = (input, coinsSymbolList, coinsIdList) => __awaiter(void 0, void 0, void 0, function* () {
+    return new Promise(() => {
+        var isValid = false;
+        let i;
+        for (i = 0; i < coinSymbolList.length; i++) {
+            if (input == coinsIdList[i].toLowerCase()) {
+                isValid = true;
+                input = coinsSymbolList[i].toLowerCase();
+                return input;
+            }
+            else if (input == coinsSymbolList[i].toLowerCase()) {
+                isValid = true;
+                return input;
+            }
+        }
+        if (isValid == false) {
+            console.log('Input is not valid cryptocurrency/token/coin.\n');
+            process.exit(0);
+        }
+    });
+});
+//collects list of all exchanges and returns exchangesList (Array with list of all exchanges)
+var exchangeList = (exchangesList = new Array()) => __awaiter(void 0, void 0, void 0, function* () {
+    const coingecko = require('coingecko-api');
+    const ciongeckoclient = new coingecko();
     let res = yield ciongeckoclient.exchanges.list();
     exchangesList = res.data;
-    exchangesNumber = exchangesList.length;
+    return exchangesList;
 });
-var coinList = () => __awaiter(void 0, void 0, void 0, function* () {
+//collects list of all coins and returns coinsList (Array with list of all coins)
+var coinIdList = () => __awaiter(void 0, void 0, void 0, function* () {
+    const coingecko = require('coingecko-api');
+    const ciongeckoclient = new coingecko();
     let res = yield ciongeckoclient.coins.list();
-    coinsList = res.data;
-    coinsNumber = coinsList.length;
+    let i;
+    var coinsList = new Array();
+    for (i = 0; i < res.data.length; i++) {
+        coinsList[i] = res.data[i].id.toString();
+    }
+    return coinsList;
 });
-var result = (index) => {
-    var res = exchangesList[index];
-    resultExchange = res.id;
-};
-//logic
-var logic = () => __awaiter(void 0, void 0, void 0, function* () {
+var coinSymbolList = () => __awaiter(void 0, void 0, void 0, function* () {
+    const coingecko = require('coingecko-api');
+    const ciongeckoclient = new coingecko();
+    let res = yield ciongeckoclient.coins.list();
+    let i;
+    var coinsList = new Array();
+    for (i = 0; i < res.data.length; i++) {
+        coinsList[i] = res.data[i].symbol.toString();
+    }
+    return coinsList;
+});
+//logic -> TODO
+var logic = (resultPrice, resultExchange, currencyId, currencySymbol) => __awaiter(void 0, void 0, void 0, function* () {
+    const coingecko = require('coingecko-api');
+    const ciongeckoclient = new coingecko();
     var price = 0;
     var exchange = "";
     var atLeastOneExchange = false;
     var allCoinTickers = new Array();
     var coinTickers = () => __awaiter(void 0, void 0, void 0, function* () {
-        var res = yield ciongeckoclient.coins.fetchTickers(curency);
+        var res = yield ciongeckoclient.coins.fetchTickers(currencyId);
         console.log(res.tickers);
-        allCoinTickers = res.tickers;
-        //console.log(allCoinTickers[0]);
+        allCoinTickers = JSON.parse(res.tickers);
     });
     coinTickers();
     var tickersNumber = 0;
-    /*tickersNumber = allCoinTickers.length;
-  
+    tickersNumber = allCoinTickers.length;
     let i;
-    for(i = 0; i < tickersNumber; i++)
-    {
-  
-      if(allCoinTickers[i].base == curency && (allCoinTickers[i].target == 'USD' || allCoinTickers[i].target == 'USDT'))
-      {
-        atLeastOneExchange = true;
-        price = allCoinTickers[i].last;
-        exchange = allCoinTickers[i].market.name;
-  
-        if(price > resultPrice)
-        {
-          resultPrice = price;
-          resultExchange = exchange;
+    for (i = 0; i < tickersNumber; i++) {
+        if (allCoinTickers[i].base == currencySymbol && (allCoinTickers[i].target == 'USD' || allCoinTickers[i].target == 'USDT')) {
+            atLeastOneExchange = true;
+            price = allCoinTickers[i].last;
+            exchange = allCoinTickers[i].market.name;
+            if (price > resultPrice) {
+                resultPrice = price;
+                resultExchange = exchange;
+            }
         }
-  
-      }
-      
     }
-  
-    if(atLeastOneExchange = false)
-    {
-      console.log('There is no exchange that ofers transaction ' + curency + ' to USD.\n');
-    }*/
+    if (atLeastOneExchange = false) {
+        console.log('There is no exchange that ofers transaction ' + currencyId + ' to USD.\n');
+    }
 });
 //output of results
-var output = () => {
+var output = (resultExchange, resultPrice) => {
     console.log(resultExchange + ' ' + resultPrice + '$' + '\n');
 };
 //main function
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
-    yield exchangeList();
-    yield coinList();
-    yield input();
-    //await inputCheck(curency);
-    yield logic();
-    //console.log(coinsList);
-    rl.close();
-    output();
+    var resultPrice = 0;
+    var resultExchange = "";
+    //var listOfExchanges = await exchangeList();
+    var currency = yield input();
+    var listOfCoinsId = new Array();
+    listOfCoinsId = yield coinIdList();
+    var listOfCoinsSymbol = new Array();
+    listOfCoinsSymbol = yield coinSymbolList();
+    var coinId = yield inputIdCheck(currency, listOfCoinsId, listOfCoinsSymbol);
+    var coinSymbol = yield inputSymbolCheck(currency, listOfCoinsSymbol, listOfCoinsId);
+    yield logic(resultPrice, resultExchange, coinId, coinSymbol);
+    output(resultExchange, resultPrice);
 });
 //execution
 main();
--- src/app.ts
+++ src/app.ts
@@ -1,106 +1,149 @@
 #!/usr/bin/env node
 
-'use strict'
-
 //token/coin/cryptocurrency input
-var input = () => {
+    
+
+
+const input = async() => {
+  
+  'use strict'
+
   var curency:string = "";
-  const inputInner = () => {
-    return new Promise<void>((resolve, reject) => {
-      const readline = require('readline')
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-          })
-      rl.question('Please enter the name of a token/coin/cryptocurrency (ID or symbol)\n or \"quit\" to exit program.\n\n\n', (answer:string) => {
-        if(answer == 'quit') process.exit(0);
-        curency = answer.toLowerCase();
-        resolve();
+
+  const readline = require('readline')
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+      const questionn = () => {
+       return new Promise<void>((resolve, reject) => {
+        rl.question('Please enter the name of a token/coin/cryptocurrency (ID or symbol)\n or \"quit\" to exit program.\n\n\n', (answer:string) => {
+          if(answer == 'quit') { process.exit(0);};
+          curency = answer.toLowerCase();
+          resolve();
+       })
       })
-      rl.close();
-    })
-  }
+    }
+
+  console.log(await questionn());
+  rl.close();
   return curency;
 }
     
 
 //checks if input is valid and returns coin Id
-const inputIdCheck = async (input:String, coinsList:any) => {
+const inputIdCheck = async (input:String, coinsIdList:Array<string>, coinsSymbolList:Array<string>) => {
+  return new Promise<string>(() => {
+
     var isValid:Boolean = false;
     let i:number;
-    for(i = 0; i < coinList.length; i++)
+
+    for(i = 0; i < coinsIdList.length; i++)
     {
-        if(input == coinsList[i].symbol.toLowerCase())
+        if(input == coinsSymbolList[i].toLowerCase())
         {
           isValid = true;
-          input = coinsList[i].id.toLowerCase();
+          input = coinsIdList[i].toLowerCase();
           return input;
         }
-        else if(input == coinsList[i].id.toLowerCase())
+        else if(input == coinsIdList[i].toLowerCase())
         {
           isValid = true;
           return input;
         }
     }
-    if(isValid = false)
+    if(isValid == false)
     {
       console.log('Input is not valid cryptocurrency/token/coin.\n');
-      return 'error';
+      return '';
     }
+  })
+
 }
 
 //checks if input is correct and returns coin symbol
-const inputSymbolCheck = async (input:String, coinsList:any) => {
-  var isValid:Boolean = false;
-  let i:number;
-  for(i = 0; i < coinList.length; i++)
-  {
-      if(input == coinsList[i].Id.toLowerCase())
-      {
-        isValid = true;
-        input = coinsList[i].symbol.toLowerCase();
-        return input;
-      }
-      else if(input == coinsList[i].symbol.toLowerCase())
-      {
-        isValid = true;
-        return input;
-      }
-  }
-  if(isValid = false)
-  {
-    console.log('Input is not valid cryptocurrency/token/coin.\n');
-    return 'error';
-  }
+const inputSymbolCheck = async (input:String, coinsSymbolList:Array<string>, coinsIdList:Array<string>) => {
+  return new Promise<string>(() => {
+
+    var isValid:Boolean = false;
+    let i:number;
+  
+    for(i = 0; i < coinSymbolList.length; i++)
+    {
+        if(input == coinsIdList[i].toLowerCase())
+        {
+          isValid = true;
+          input = coinsSymbolList[i].toLowerCase();
+          return input;
+        }
+        else if(input == coinsSymbolList[i].toLowerCase())
+        {
+          isValid = true;
+          return input;
+        }
+    }
+    if(isValid == false)
+    {
+      console.log('Input is not valid cryptocurrency/token/coin.\n');
+      process.exit(0);
+    }  
+  })
+  
 }
 
 //collects list of all exchanges and returns exchangesList (Array with list of all exchanges)
-var exchangeList = async(exchangesList = new Array()) => {
+// var exchangeList = async(exchangesList = new Array()) => {
 
-  const coingecko = require('coingecko-api');
-  const ciongeckoclient = new coingecko();
+//   const coingecko = require('coingecko-api');
+//   const ciongeckoclient = new coingecko();
 
-    let res = await ciongeckoclient.exchanges.list();
-    exchangesList = res.data;
-    return exchangesList;
-}
+//     let res = await ciongeckoclient.exchanges.list();
+//     exchangesList = res.data;
+//     return exchangesList;
+// }
 
 //collects list of all coins and returns coinsList (Array with list of all coins)
-var coinList = async(coinsList = new Array()) => {
+var coinIdList = async() => {
 
   const coingecko = require('coingecko-api');
   const ciongeckoclient = new coingecko();
 
   let res = await ciongeckoclient.coins.list();
-  coinsList = res.data;
+  let i:number;
+  var coinsList = new Array();
+  for(i = 0; i < res.data.length; i++)
+  {
+    coinsList[i] = res.data[i].id.toString();
+  }
+  return coinsList;
+}
+
+var coinSymbolList = async() => {
+
+  const coingecko = require('coingecko-api');
+  const ciongeckoclient = new coingecko();
+
+  let res = await ciongeckoclient.coins.list();
+  let i:number;
+  var coinsList = new Array();
+  for(i = 0; i < res.data.length; i++)
+  {
+    coinsList[i] = res.data[i].symbol.toString();
+  }
   return coinsList;
 }
 
   //logic -> TODO
-  var logic = async(resultPrice:number, resultExchange:string, currencyId:any, currencySymbol:any) => {
-
+  var logic = async(currencyId:any, currencySymbol:any) => {
+    return new Promise<Array<any>> (() => {
     const coingecko = require('coingecko-api');
     const ciongeckoclient = new coingecko();
+
+    var result = new Array();
+
+    var currentPrice:number = 0;
+    var currentExchenage = "";
 
     var price:number = 0;
     var exchange:string = "";
@@ -130,10 +173,10 @@ var coinList = async(coinsList = new Array()) => {
         price = allCoinTickers[i].last;
         exchange = allCoinTickers[i].market.name;
 
-        if(price > resultPrice)
+        if(price > currentPrice)
         {
-          resultPrice = price;
-          resultExchange = exchange;
+          currentPrice = price;
+          currentExchenage = exchange;
         }
 
       }
@@ -143,8 +186,14 @@ var coinList = async(coinsList = new Array()) => {
     if(atLeastOneExchange = false)
     {
       console.log('There is no exchange that ofers transaction ' + currencyId + ' to USD.\n');
+      process.exit(0);
     }
+
+    result[0] = currentExchenage;
+    result[1] = currentPrice;
+    return result;
   }
+)}  
 
 //output of results
 var output = (resultExchange:string, resultPrice:number) => {
@@ -153,15 +202,20 @@ var output = (resultExchange:string, resultPrice:number) => {
 
 //main function
 const main = async () => {
-  var resultPrice:number = 0;
-  var resultExchange:string = "";
+  var result = new Array();
+  result[0] = ""; //result exchange
+  result[1] = 0; //result price
   //var listOfExchanges = await exchangeList();
-  var listOfCoins = await coinList();
   var currency:string = await input();
-  var coinId = await inputIdCheck(currency, coinList);
-  var coinSymbol = await inputSymbolCheck(currency, coinList);
-  await logic(resultPrice, resultExchange, coinId, coinSymbol);
-  output(resultExchange, resultPrice);
+  var listOfCoinsId = new Array<string>();
+  listOfCoinsId = await coinIdList();
+  var listOfCoinsSymbol = new Array<string>()
+  listOfCoinsSymbol = await coinSymbolList();
+  var coinId:string = await inputIdCheck(currency, listOfCoinsId, listOfCoinsSymbol);
+  var coinSymbol:string = await inputSymbolCheck(currency, listOfCoinsSymbol, listOfCoinsId);
+  result = await logic(coinId, coinSymbol);
+
+  output(result[0], result[1]);
   }
 
 
